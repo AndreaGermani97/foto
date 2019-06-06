01d46d6952c16469bc49bae77a4adb14ec4a857a
Andrea <andrea.germani97@gmail.com>
2019-06-06 16:01:43 +0200

    Aggiunto progetto

---
+++ foto/src/main/java/it/uniroma3/siw/foto/FotoApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.foto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FotoApplication.class, args);
+	}
+
+}
