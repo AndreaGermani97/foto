7ad9777452e85d43083b781b0b8e3ddfed899f0d
Andrea <andrea.germani97@gmail.com>
2019-06-14 18:47:24 +0200

    Corretta e terminata implementazione invio richiesta

--- foto/src/main/java/it/uniroma3/siw/foto/controller/RichiestaController.java
+++ foto/src/main/java/it/uniroma3/siw/foto/controller/RichiestaController.java
@@ -1,21 +1,25 @@
 package it.uniroma3.siw.foto.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
+import it.uniroma3.siw.foto.model.Fotografia;
 import it.uniroma3.siw.foto.model.Richiesta;
 import it.uniroma3.siw.foto.service.RichiestaService;
 
 @Controller
-@Scope("session")
+@SessionAttributes("richiesta")
 public class RichiestaController {
 
 	@Autowired
@@ -24,17 +28,22 @@ public class RichiestaController {
 	@Autowired
 	private RichiestaValidator richiestaValidator;
 
+	@Autowired
+	private Carrello carrello;
+
 	@RequestMapping(value = "/richiesta", method = RequestMethod.POST)
 	public String newRichiesta(@Valid @ModelAttribute("richiesta") Richiesta richiesta, Model model,
 			BindingResult bindingResult) {
 		// controllo se la richiesta è valida:
 		// se è valida lo mando alla conferma per la persistenza nel db
-		// se non è valida lo rimando alla pagina di richiesta
+		// se non è valida lo rimando alla pagina di richiestaInoltrata
+		List<Fotografia> fotografie = new ArrayList<>(this.carrello.getFotografie().values());
+		richiesta.setFotografie(fotografie);
 		this.richiestaValidator.validate(richiesta, bindingResult);
-		if (bindingResult.hasErrors()) {
-			return "richiesta.html";
-		} else {
+		if (!bindingResult.hasErrors()) {
 			return "confermaRichiesta.html";
+		} else {
+			return "richiestaForm.html";
 		}
 
 	}
@@ -42,8 +51,15 @@ public class RichiestaController {
 	@RequestMapping(value = "/confermaRichiesta", method = RequestMethod.GET)
 	public String getRichiesta(@Valid @ModelAttribute("richiesta") Richiesta richiesta, Model model) {
 		this.richiestaService.inserisci(richiesta);
+		this.carrello.svuota(); //svuoto il carrello dopo l'invio della richiesta
 		return "richiestaInoltrata.html";
 
+	}
+	
+	@RequestMapping("/inviaRichiesta")
+	public String inviaRichiesta(Model model) {
+		model.addAttribute("richiesta", new Richiesta());
+		return "richiestaForm.html";
 	}
 
 }
--- foto/src/main/java/it/uniroma3/siw/foto/controller/RichiestaValidator.java
+++ foto/src/main/java/it/uniroma3/siw/foto/controller/RichiestaValidator.java
@@ -1,27 +1,31 @@
 package it.uniroma3.siw.foto.controller;
 
+import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 import it.uniroma3.siw.foto.model.Richiesta;
 
+@Component
 public class RichiestaValidator implements Validator {
 	
 	@Override
 	public boolean supports(Class<?> clazz) {
-		// TODO Auto-generated method stub
 		return Richiesta.class.equals(clazz);
 	}
 
 	@Override
 	public void validate(Object target, Errors errors) {
-		// TODO Auto-generated method stub
-		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "nome", "required");
-		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "cognome", "required");
-		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "email", "required");
-		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "indirizzo", "required");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "cliente.nome", "required");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "cliente.cognome", "required");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "cliente.email", "required");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "cliente.indirizzo", "required");
 		
+		Richiesta richiesta= (Richiesta) target;
+		if(richiesta.getFotografie().isEmpty()) {
+			errors.rejectValue("fotografie", "required");
+		}
 	}
 
 }
