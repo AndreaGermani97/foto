28f3cdc469008f6996b4fcc011acd74981a837cd
Enzo Chou <christ8818@hotmail.it>
2019-06-16 16:01:02 +0200

    aggiunte le immagini associate alle fotografie

--- foto/src/main/java/it/uniroma3/siw/foto/repository/DBPopulation.java
+++ foto/src/main/java/it/uniroma3/siw/foto/repository/DBPopulation.java
@@ -1,11 +1,13 @@
 package it.uniroma3.siw.foto.repository;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
 import it.uniroma3.siw.foto.model.Album;
@@ -37,7 +39,7 @@ public class DBPopulation implements ApplicationRunner{
 		this.addAll();
 	}
 	
-	private void creatoreFotografi() {
+	private void creatoreFotografi() throws IOException {
 		Fotografo putin = new Fotografo("Vladimir", "Putin");
 		
 		Album albumUnoFotografoPutin = new Album("album uno Putin", "album uno di putin");
@@ -45,15 +47,19 @@ public class DBPopulation implements ApplicationRunner{
 		Album albumDueFotografoPutin = new Album("album due Putin","album due di putin");
 		putin.addAlbum(albumDueFotografoPutin);
 		
-		Fotografia fotoUnoAlbumUnoPutin = new Fotografia("foto uno album uno putin", "");
-		Fotografia fotoDueAlbumUnoPutin = new Fotografia("foto due album uno putin", "");
-		albumUnoFotografoPutin.addFotografia(fotoUnoAlbumUnoPutin);
-		albumUnoFotografoPutin.addFotografia(fotoDueAlbumUnoPutin);
+		Fotografia putinAlbumUnoFotoUno = new Fotografia("foto uno album uno putin", "");
+		putinAlbumUnoFotoUno.setPicLocation("/images/putinAlbumUnoFotoUno.jpg");
+		Fotografia putinAlbumUnoFotoDue = new Fotografia("foto due album uno putin", "");
+		putinAlbumUnoFotoDue.setPicLocation("/images/putinAlbumUnoFotoDue.jpg");
+		albumUnoFotografoPutin.addFotografia(putinAlbumUnoFotoUno);
+		albumUnoFotografoPutin.addFotografia(putinAlbumUnoFotoDue);
 		
-		Fotografia fotoUnoAlbumDuePutin = new Fotografia("foto uno album due putin", "");
-		Fotografia fotoDueAlbumDuePutin = new Fotografia("foto due album due putin", "");
-		albumDueFotografoPutin.addFotografia(fotoUnoAlbumDuePutin);
-		albumDueFotografoPutin.addFotografia(fotoDueAlbumDuePutin);
+		Fotografia putinAlbumDueFotoUno = new Fotografia("foto uno album due putin", "");
+		putinAlbumDueFotoUno.setPicLocation("/images/putinAlbumDueFotoUno.jpg");
+		Fotografia putinAlbumDueFotoDue = new Fotografia("foto due album due putin", "");
+		putinAlbumDueFotoDue.setPicLocation("/images/putinAlbumDueFotoDue.jpg");
+		albumDueFotografoPutin.addFotografia(putinAlbumDueFotoUno);
+		albumDueFotografoPutin.addFotografia(putinAlbumDueFotoDue);
 		
 		Fotografo trump = new Fotografo("Donald", "Trump");
 
@@ -62,15 +68,19 @@ public class DBPopulation implements ApplicationRunner{
 		Album albumDueFotografoTrump = new Album("album due trump", "album due di trump");
 		trump.addAlbum(albumDueFotografoTrump);
 
-		Fotografia fotoUnoAlbumUnoTrump = new Fotografia("foto uno album uno trump", "");
-		Fotografia fotoDueAlbumUnoTrump = new Fotografia("foto due album uno trump", "");
-		albumUnoFotografoTrump.addFotografia(fotoUnoAlbumUnoTrump);
-		albumUnoFotografoTrump.addFotografia(fotoDueAlbumUnoTrump);	
+		Fotografia trumpAlbumUnoFotoUno = new Fotografia("foto uno album uno trump", "");
+		trumpAlbumUnoFotoUno.setPicLocation("/images/trumpAlbumUnoFotoUno.jpg");
+		Fotografia trumpAlbumUnoFotoDue = new Fotografia("foto due album uno trump", "");
+		trumpAlbumUnoFotoDue.setPicLocation("/images/trumpAlbumUnoFotoDue.jpg");
+		albumUnoFotografoTrump.addFotografia(trumpAlbumUnoFotoUno);
+		albumUnoFotografoTrump.addFotografia(trumpAlbumUnoFotoDue);	
 		
-		Fotografia fotoUnoAlbumDueTrump = new Fotografia("foto uno album due trump", "");
-		Fotografia fotoDueAlbumDueTrump = new Fotografia("foto due album due trump", "");
-		albumDueFotografoTrump.addFotografia(fotoUnoAlbumDueTrump);
-		albumDueFotografoTrump.addFotografia(fotoDueAlbumDueTrump);
+		Fotografia trumpAlbumDueFotoUno = new Fotografia("foto uno album due trump", "");
+		trumpAlbumDueFotoUno.setPicLocation("/images/trumpAlbumDueFotoUno.jpg");
+		Fotografia trumpAlbumDueFotoDue = new Fotografia("foto due album due trump", "");
+		trumpAlbumDueFotoDue.setPicLocation("/images/trumpAlbumDueFotoDue.jpg");
+		albumDueFotografoTrump.addFotografia(trumpAlbumDueFotoUno);
+		albumDueFotografoTrump.addFotografia(trumpAlbumDueFotoDue);
 		
 		
 		Dipendente dipendenteEnzo = new Dipendente("enzotongxing.chou@gmail.com", "bananabanana", "Enzo", "Chou", "dipendente");
@@ -79,9 +89,9 @@ public class DBPopulation implements ApplicationRunner{
 		Cliente obama = new Cliente("Barack", "Obama", "BarackObama@gmail.com", "casa bianca");
 		Cliente clinton	= new Cliente("Bill", "Clinton", "clintonBill@gmail.com", "casa bianca");
 		List<Fotografia> richiestaObama = new ArrayList<>();
-		richiestaObama.add(fotoUnoAlbumUnoPutin);
+		richiestaObama.add(putinAlbumUnoFotoUno);
 		List<Fotografia> richiestaClinton = new ArrayList<>();
-		richiestaClinton.add(fotoDueAlbumUnoPutin);
+		richiestaClinton.add(putinAlbumUnoFotoDue);
 		Richiesta richiestaUno = new Richiesta(obama, richiestaObama);
 		Richiesta richiestaDue = new Richiesta(clinton, richiestaClinton);
 		
@@ -93,7 +103,7 @@ public class DBPopulation implements ApplicationRunner{
 		richieste.add(richiestaDue);
 	}
 
-	private void addAll() {
+	private void addAll() throws IOException {
 		creatoreFotografi();
 		fotografoRepository.saveAll(fotografi);
 		dipendenteRepository.saveAll(dipendenti);
